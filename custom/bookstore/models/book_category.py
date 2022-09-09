864b59ebcae5b00229011f0abd8e672dcb0d94cc
Diki Hamdani <dikihamdani.id@gmail.com>
2022-09-09 15:14:00 +0700

    Update: add python constraints to book category models

--- custom/bookstore/models/book_category.py
+++ custom/bookstore/models/book_category.py
@@ -1,21 +1,52 @@
-from ast import Lambda
-from unittest import result
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class BookCategory(models.Model):
     _name = 'bookstore.bookcategory'
     _description = 'Kategori Buku'
 
-    name = fields.Char(string='Nama Kategori')
+    CHOICES = [
+        ('antropologi', 'Antropologi'),
+        ('astronomi', 'Astronomi'),
+        ('biografi', 'Biografi'),
+        ('bisnis', 'Bisnis'),
+        ('ekonomi', 'Ekonomi'),
+        ('etika', 'Etika'),
+        ('fiksi', 'Fiksi'),
+        ('komputer', 'Komputer'),
+        ('masak', 'Masak'),
+        ('medis', 'Medis'),
+        ('musik', 'Musik'),
+        ('pemasaran', 'Pemasaran'),
+        ('pengembangan_diri', 'Pengembangan Diri'),
+        ('psikologi', 'Psikologi'),
+        ('sasrtra', 'Sastra'),
+        ('sejarah', 'Sejarah'),
+        ('sosiologi', 'Sosiologi'),
+        ('dongeng', 'Dongeng'),
+        ('fotografi', 'Fotografi'),
+        ('nonfiksi', 'Nonfiksi'),
+    ]
+
+    name = fields.Selection(string='Nama Kategori',
+                            selection=CHOICES,
+                            required=True)
     category_code = fields.Char(string='Kode Kategori',
                                 readonly=True,
                                 copy=False,
                                 required=True,
                                 default='New')
-    book_ids = fields.One2many(comodel_name='bookstore.book', inverse_name='category_id', string='Daftar Buku')
+    book_ids = fields.One2many(comodel_name='bookstore.book',
+                                inverse_name='category_id',
+                                string='Daftar Buku',
+                                required=True)
     
-    
+    @api.constrains('name')
+    def check_unique_name(self):
+        count = self.search_count([('name', '=', self.name), ('id', '!=', self.id)])
+        if count > 0:
+            raise ValidationError('Kategori {} sudah ada di Database!'.format(self.name))
     
     @api.model
     def create(self, vals):
